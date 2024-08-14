3e9d520eb0bbf38e8e1e5a86a77cf4b8a3176751
byron1992 <bybolade@gmail.com>
2024-08-13 22:09:20 -0500

    funcion encriptar completa

--- codigo.js
+++ codigo.js
@@ -1,23 +1,25 @@
 function encriptar(){
-    let texto = document.getElementById("texto_a_encriptar").value;
+    let texto = document.getElementById("texto_a_cambiar").value;
     let tituloMensaje = document.getElementById("titulo_mensaje");
     let parrafo = document.getElementById("parrafo");
-    let muñeco  = document.getElementById("muneco");
+    let muñeco  = document.getElementById("muneco"); 
 
-    let textoCifrado=texto
-                        .replace(/a/gi, "ai")
+    let textoCifrado=texto /* aqui el texto que recibí del textarea lo estoy cambiando según las indicaciones del reto */
                         .replace(/e/gi, "enter")
                         .replace(/i/gi, "imes")
+                        .replace(/a/gi, "ai")
                         .replace(/o/gi, "ober")
                         .replace(/u/gi, "ufat");
 
-                    if (texto.length!=0){
-                        texto=textoCifrado;
+                    if (document.getElementById("texto_a_cambiar").value.length!=0){/*  verifico que ingrese texto */
+                        document.getElementById("texto_a_cambiar").value=textoCifrado;
                         tituloMensaje.textContent = "El texto ha sido encriptado";
                         parrafo.textContent="";
                         muñeco.src = "./imagenes/encriptado.jpg";
                      } else {
-                       muñeco.src = "./imagenes/Muneco.png";
+                        muñeco.src = "./imagenes/Muneco.png";
+                        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+                        parrafo.textContent="Ingresa el texto que desea encriptar o desencriptar";
                        alert("Debes ingresar texto");
                      }
 }
